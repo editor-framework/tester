65e5b3517135d37ce14f552259f0348f3875d5f9
Johnny Wu <KarasAya@gmail.com>
2015-05-28 03:01:50 +0800

    update

--- panel/reporter.js
+++ panel/reporter.js
@@ -23,24 +23,55 @@ function IpcReporter(runner) {
 
     var self = this, stats = this.stats, n = -1;
 
-    runner.on('start', function(){
+    runner.on('start', function () {
         Ipc.sendToHost('runner:start');
     });
 
-    runner.on('pending', function(test){
+    runner.on('suite', function(suite){
+        // if (suite.root) return;
+
+        // // suite
+        // var url = self.suiteURL(suite);
+        // var el = fragment('<li class="suite"><h1><a href="%s">%s</a></h1></li>', url, escape(suite.title));
+
+        // // container
+        // stack[0].appendChild(el);
+        // stack.unshift(document.createElement('ul'));
+        // el.appendChild(stack[0]);
+
+        Ipc.sendToHost('runner:suite', stats, suite);
+    });
+
+    runner.on('suite end', function (suite) {
+        // if (suite.root) return;
+        // stack.shift();
+        Ipc.sendToHost('runner:suite-end', stats, suite);
+    });
+
+    runner.on('test', function(test) {
+        Ipc.sendToHost('runner:test', stats, test);
+    });
+
+    runner.on('pending', function (test) {
         Ipc.sendToHost('runner:pending', test);
     });
 
-    runner.on('pass', function(test){
+    runner.on('pass', function (test) {
         Ipc.sendToHost('runner:pass', test);
     });
 
-    runner.on('fail', function(test, err){
-        Ipc.sendToHost('runner:fail', test);
+    runner.on('fail', function (test, err) {
+        Ipc.sendToHost('runner:fail', test, err);
     });
 
-    runner.on('end', function(){
-        Ipc.sendToHost('runner:finish');
+    runner.on('test end', function(test) {
+        stats.duration = new Date() - stats.start;
+        stats.progress = stats.tests / this.total * 100 | 0;
+        Ipc.sendToHost('runner:test-end', stats, test);
+    });
+
+    runner.on('end', function () {
+        Ipc.sendToHost('runner:end');
         self.epilogue();
     });
 }
--- panel/tester.js
+++ panel/tester.js
@@ -7,6 +7,19 @@ Editor.registerPanel( 'tester.panel', {
     },
 
     ready: function () {
+        this.startRunner();
+    },
+
+    startRunner: function () {
+        this.passes = 0;
+        this.failures = 0;
+        this.duration = 0;
+        this.progress = 0;
+
+        this.suites = [];
+
+        this._curSuite = null;
+        this._curSuiteIdx = -1;
     },
 
     _onRunnerConsole: function ( event ) {
@@ -14,24 +27,65 @@ Editor.registerPanel( 'tester.panel', {
     },
 
     _onRunnerIpc: function ( event ) {
+        var stats, suite, test, err;
+
         switch ( event.channel ) {
         case 'runner:start':
             console.log('runner start');
             break;
 
+        case 'runner:suite':
+            stats = event.args[0];
+            suite = event.args[1];
+
+            if ( suite.root ) return;
+
+            this._curSuite = {
+                title: suite.title,
+                tests: [],
+            };
+            this._curSuiteIdx = this.suites.length;
+            this.push('suites', this._curSuite);
+
+            break;
+
+        case 'runner:suite-end':
+            stats = event.args[0];
+            suite = event.args[1];
+
+            if ( suite.root ) return;
+
+            break;
+
+        case 'runner:test':
+            break;
+
+        case 'runner:test-end':
+            stats = event.args[0];
+            test = event.args[1];
+            this.passes = stats.passes;
+            this.failures = stats.failures;
+            this.duration = (stats.duration / 1000).toFixed(2);
+            this.progress = stats.progress;
+
+            this.push('suites.' + this._curSuiteIdx + '.tests', {
+                title: test.title,
+                speed: test.speed,
+                duration: test.duration,
+            });
+
+            break;
+
         case 'runner:pending':
-            console.log(event);
             break;
 
         case 'runner:pass':
-            console.log(event);
             break;
 
         case 'runner:fail':
-            console.log(event);
             break;
 
-        case 'runner:finish':
+        case 'runner:end':
             console.log('runner finish');
             break;
         }
